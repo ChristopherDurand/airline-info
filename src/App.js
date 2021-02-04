7a44cc8cf2ea30903024e7f1e837a15edf21fbce
ChristopherDurand <fonsuder@gmail.com>
2021-02-04 17:24:39 -0600

    App displays raw data in table form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Data from './data';
 
-const App = () => (
-  <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+const App = () => {
+  const key = route => `${route.airline}${route.src}${route.dest}`
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <table className='routes-table'>
+        <thead>
+          <tr>
+            <th>Airline</th>
+            <th>Source</th>
+            <th>Destination</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Data.routes.map(route =>
+            <tr key={key(route)}>
+              <td>{route.airline}</td>
+              <td>{route.src}</td>
+              <td>{route.dest}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+};
 
 export default App;
