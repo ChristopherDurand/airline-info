792978d0017fe9d5af053b2c558d35025508e415
ChristopherDurand <fonsuder@gmail.com>
2021-02-05 08:13:11 -0600

    initial stages of pagination -- app shows only the first 25 entries

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Table = ({ 
   className,  
@@ -6,6 +6,10 @@ const Table = ({
   rows, 
   format 
 }) => {
+  const [page, setPage] = useState(1);
+  const ENTRIES_PER_PAGE = 25;
+  const maxPages = Math.ceil(columns.length / ENTRIES_PER_PAGE);
+  const shownRows = rows.slice((page - 1) * ENTRIES_PER_PAGE, page * ENTRIES_PER_PAGE);
   return (
   <table className='routes-table'>
     <thead>
@@ -16,7 +20,7 @@ const Table = ({
       </tr>
     </thead>
     <tbody>
-      {rows.map(row =>
+      {shownRows.map(row =>
         <tr key={Object.values(row).join("-")}>
           {columns.map(({ property }) => 
             <td key={property}>{format(property, row[property])}</td>
