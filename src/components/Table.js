eee59abee1df5d3e57cff80eea4c2584d8f071a9
ChristopherDurand <fonsuder@gmail.com>
2021-02-05 08:19:15 -0600

    app now shows what entries are shown at the bottom of the page

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,31 +4,36 @@ const Table = ({
   className,  
   columns,
   rows, 
-  format 
+  format,
+  perPage
 }) => {
   const [page, setPage] = useState(1);
-  const ENTRIES_PER_PAGE = 25;
-  const maxPages = Math.ceil(columns.length / ENTRIES_PER_PAGE);
-  const shownRows = rows.slice((page - 1) * ENTRIES_PER_PAGE, page * ENTRIES_PER_PAGE);
+  const maxPages = Math.ceil(columns.length / perPage);
+  const shownRows = rows.slice((page - 1) * perPage, page * perPage);
   return (
-  <table className='routes-table'>
-    <thead>
-      <tr>
-        {columns.map(column => 
-          <th key={column.name}>{column.name}</th>
-        )}
-      </tr>
-    </thead>
-    <tbody>
-      {shownRows.map(row =>
-        <tr key={Object.values(row).join("-")}>
-          {columns.map(({ property }) => 
-            <td key={property}>{format(property, row[property])}</td>
+  <section>
+    <table className={className}>
+      <thead>
+        <tr>
+          {columns.map(column => 
+            <th key={column.name}>{column.name}</th>
           )}
         </tr>
-      )}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {shownRows.map(row =>
+          <tr key={Object.values(row).join("-")}>
+            {columns.map(({ property }) => 
+              <td key={property}>{format(property, row[property])}</td>
+            )}
+          </tr>
+        )}
+      </tbody>
+    </table>
+    <div className='pagination'>
+      <p>Showing {(page-1) * perPage + 1}-{page*perPage} of {rows.length} routes.</p>
+    </div>
+   </section>
   )
 }
 export default Table;
